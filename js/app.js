a44ad36f573d00022d56dbacd94f60bd15b151c3
Shavqat <71512034+Shavqat001@users.noreply.github.com>
2024-10-10 11:04:34 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('.main-header__link a').forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+let i = 45;
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        i += 5;
+    } else {
+        i -= 5;
+    }
+
+    document.querySelector('.cube-1').style = `transform: rotateX(60deg) rotate(${i}deg)`;
+    document.querySelector('.cube-2').style = `transform: rotateX(60deg) rotate(${i}deg)`;
+    lastScrollTop = scrollTop;
+});
